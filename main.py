9f5b31e380e88fcb85068d21b53da4ddde1a4a91
Ahmad Zainul Mufid <ahmadzainulmufid12@gmail.com>
2025-08-03 14:40:38 +0700

    task 2 clear

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import glob
+
+#data preprocessing & menggabungkan semua file CSV
+all_files = glob.glob("data/*.csv")
+df_list = [pd.read_csv(file) for file in all_files]
+df = pd.concat(df_list, ignore_index=True)
+
+#filtering data untuk produk 'Pink Morsel'
+df = df[df['product'] == 'pink morsel']
+
+#menghitung total penjualan
+df['sales'] = df['quantity'] * df['price']
+
+#mengambil kolom yang diperlukan
+final_df = df[['sales', 'date', 'region']]
+final_df = final_df.rename(columns={'date': 'date', 'region': 'region'})
+
+#menyimpan hasil akhir ke file CSV
+final_df.to_csv("formated_output.csv", index=False)
